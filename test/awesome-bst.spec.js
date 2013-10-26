31577c91c7bd75f5e0ecabbde4e3618836da0b0a
justin j. moses <justinjmoses@gmail.com>
2013-10-26 14:53:13 -0400

    Initial work - `add` and `size` methods

---
+++ test/awesome-bst.spec.js
@@ -0,0 +1,31 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+var BST = require('../lib/awesome-bst');
+
+describe('BST', function() {
+   
+    var bst = new BST.AwesomeBST();
+    
+    describe('size()', function() { 
+        it('handles empty case', function() {
+            expect(bst.size()).to.equal(0);
+        });
+        it('increments as expected', function() {
+            bst.add(12); 
+            expect(bst.size()).to.equal(1);
+            bst.add(2);
+            expect(bst.size()).to.equal(2);
+        });
+    });
+
+    describe('From Array', function() { 
+        
+        it('takes array input', function() { 
+            bst.loadFrom([12,4,5,19,3]);
+            //expect(bst.size()).to.equal(5);
+        });
+
+    });
+
+});
